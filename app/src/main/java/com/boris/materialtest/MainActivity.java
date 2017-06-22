617bb3885a415c528180247cc25f917a0da738f3
Boris <101b1@rambler.ru>
2017-06-22 17:51:44 +0300

    Added aboutFragment skeleton.

--- app/src/main/java/com/boris/materialtest/MainActivity.java
+++ app/src/main/java/com/boris/materialtest/MainActivity.java
@@ -2,12 +2,50 @@ package com.boris.materialtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private String[] drawerItems;
+    private ListView drawerList;
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            viewToast(position);
+        }
+    }
+
+    private void viewToast(int position){
+        String[] message;
+        message = getResources().getStringArray(R.array.drawer_items);
+        Toast.makeText(this, "Pressed"+message[position], Toast.LENGTH_SHORT).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        drawerItems = getResources().getStringArray(R.array.drawer_items);
+        drawerList = (ListView)findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, drawerItems));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
         setContentView(R.layout.activity_main);
     }
 }
